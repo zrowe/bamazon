182f05c77d904908c7f735a7ee18c6638c893468
zrowe <paul.louis.rowe@gmailcom>
2018-02-22 22:47:14 -0800

    adding display of products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,7 @@
 require("dotenv").config(); // get mySQL credentials
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require('cli-table');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -9,10 +11,53 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-// console.log("Selecting all items...\n");
-connection.query("SELECT * FROM products", function(err, res) {
+connection.query("SELECT item_id, product_name, department_name, price, stock_quantity FROM products", function(err, res) {
     if (err) throw err;
-    // Log all results of the SELECT statement
     connection.end();
-    console.log(res);
+    showProducts(res);
 });
+
+function showProducts(res) {
+
+    var table = new Table({
+        head: ['Item ID', 'Product', 'Department', 'Price', 'Qty Avail'],
+        colAligns: ['middle', 'left', 'left', 'right', 'middle'],
+        // colWidths: [5, 80, 25, 10, 10],
+        // chars: {
+        //     'top': '═',
+        //     'top-mid': '╤',
+        //     'top-left': '╔',
+        //     'top-right': '╗',
+        //     'bottom': '',
+        //     'bottom-mid': '',
+        //     'bottom-left': '',
+        //     'bottom-right': '',
+        //     'left': '',
+        //     'left-mid': '',
+        //     'mid': '',
+        //     'mid-mid': '',
+        //     'right': '',
+        //     'right-mid': '',
+        //     'middle': ' '
+        // },
+        style: { 'padding-left': 1, 'padding-right': 1 }
+    });
+
+
+    for (var i = 0; i < res.length; i++) {
+        var arr = Object.keys(res[i]).map(function(key) { return res[i][key]; });
+        table.push(arr);
+    }
+    // list out all the products
+    console.log(table.toString());
+    askForPurchase();
+}
+
+function askForPurchase() {
+
+    // "What is the ID of the item you would like to purchase? [Quit with a Q]"
+    // get id 
+    // if Q then quit
+    // "How many would you like? [Quite with a Q]"
+
+}
